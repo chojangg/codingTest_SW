1fea44bb812e3e9c5dc59396e6fcacaace5ad750
JAYUN <83991079+chojangg@users.noreply.github.com>
2022-09-20 08:30:47 +0900

    220916 자릿수 더하기

---
+++ addDigit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) {
+    int answer = 0;
+    while (n > 0) {
+        answer += n % 10;
+        n /= 10;
+    }
+    return answer;
+}
+
+int main(void) {
+    int num;
+    scanf("%d", &num);
+    printf("%d", solution(num));
+
+    return 0;
+}
